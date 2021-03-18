71b734723660d245c48174adad9b2b39c65d9648
Christopher Cho <swhtjddn@protonmail.com>
2021-03-18 13:34:56 -0700

    more animations horizontal scrolling fix

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ let cursor = gsap.to('.cursor', {
     repeat: -1
 });
 
+let description = gsap.to('.hero-description', {
+    alpha: 0.5,
+    duration: 1,
+    ease: "none",
+    yoyo: true,
+    repeat: -1
+})
+
 gsap.from(".hero-wrapper", {
     scrollTrigger: {
         trigger: ".hero-wrapper",
@@ -114,21 +122,12 @@ gsap.defaults({ease: "power3"});
 gsap.set(".grid-item", {y: 50});
 
 ScrollTrigger.batch(".grid-item", {
-    //interval: 0.1, // time window (in seconds) for batching to occur. 
-    //batchMax: 3,   // maximum batch size (targets)
     onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: {each: 0.15, grid: [1, 3]}, overwrite: true}),
     onLeave: batch => gsap.set(batch, {opacity: 0, y: -100, overwrite: true}),
     onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15, overwrite: true}),
     onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 100, overwrite: true})
-    // you can also define things like start, end, etc.
 });
     
-    
-    // when ScrollTrigger does a refresh(), it maps all the positioning data which 
-    // factors in transforms, but in this example we're initially setting all the ".box"
-    // elements to a "y" of 100 solely for the animation in which would throw off the normal 
-    // positioning, so we use a "refreshInit" listener to reset the y temporarily. When we 
-    // return a gsap.set() in the listener, it'll automatically revert it after the refresh()!
 ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".grid-item", {y: 0}));
 
 let contact = gsap.timeline({
@@ -151,7 +150,7 @@ let contact = gsap.timeline({
     })
     .from(".contact-form", {
         duration: 1,
-        rotation: 20,
+        rotation: 10,
         yoyo: true,
         y: "-=50",
         ease: "bounce",
